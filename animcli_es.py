884d7600ca7dea6c27eee449fb0ba567b1525296
agus-balles <agustinballesteros04@gmail.com>
2024-01-22 14:36:30 -0300

    added download option(a bit bodgy) and alternate provider for the scraper in case the main one gets blocked

--- animcli_es.py
+++ animcli_es.py
@@ -11,10 +11,11 @@ parser=argparse.ArgumentParser(description="Mira anime subtitulado en español."
 parser.add_argument("-A","--anime", type=str, help="Titulo del Anime")
 parser.add_argument("-C","--capitulo",type=int,help="Capitulo del Anime")
 parser.add_argument("-R","--reproduccion_automatica",action="store_true",help="Reproduce los siguientes episodios automáticamente.")
+parser.add_argument("-D","--descarga",action="store_true",help="Descarga este episodio, y si se selecciona, los subsiguientes")
 
 args=parser.parse_args()
 
-def watch_video(episode_list, episode_index,provider=0,passive=False):
+def watch_video(anime,episode_list, episode_index,provider=0,passive=False):
     episode_links = api.get_links(episode_index+1)
     provider_string = episode_links[episode_list[episode_index]][provider][:14]
     if passive ==True:
@@ -22,19 +23,36 @@ def watch_video(episode_list, episode_index,provider=0,passive=False):
             episode_links = api.get_links(episode_index+i+1)
             video_links=episode_links[episode_list[episode_index+i]]
             video= [link for link in video_links if provider_string in link][0]
-            print(f"Episodio {episode_index+i+1}:\n{video}")
-            os.system(f"mpv ytdl://{video}")
+            print(f"\033[1;36mEpisodio {episode_index+i+1}:\n{video}")
+            os.system(f"mpv ytdl://{video} --force-media-title=\"{anime} - Episodio: {episode_index+1}\"")
     else:
         video_links=episode_links[episode_list[episode_index]]
         video= [link for link in video_links if provider_string in link][0]
         print(f"Episodio {episode_index+1}:\n{video}")
-        os.system(f"mpv ytdl://{video}")
+        os.system(f"mpv ytdl://{video} --force-media-title=\"{anime} - Episodio {episode_index+1}\"")
 
-        
+def download_video(anime_id,episode_list,episode_index,provider=0,download_following=False):
+    if not os.path.isdir(anime_id):
+        os.mkdir(anime_id)
+    episode_links = api.get_links(episode_index+1)
+    provider_string = episode_links[episode_list[episode_index]][provider][:14]
+    if download_following ==True:
+        for i in range(len(episode_list)-episode_index):
+            episode_links = api.get_links(episode_index+i+1)
+            video_links=episode_links[episode_list[episode_index+i]]
+            video= [link for link in video_links if provider_string in link][0]
+            print(f"\033[1;36mDescargando Episodio {episode_index+i+1}:\n{video}")
+            os.system(f"yt-dlp -N 8 {video} -o {anime_id}\/{episode_list[episode_index+i]} ")
+    else:
+        video_links=episode_links[episode_list[episode_index]]
+        video= [link for link in video_links if provider_string in link][0]
+        print(f"\033[1;36mDescargando episodio {episode_index+1}:\n{video}")
+        os.system(f"yt-dlp -N 8 {video} -o {anime_id}\/{episode_list[episode_index]}")
 
 if args.anime:
     anime_id = api.search(args.anime)[0]
     animeinfo = api.anime_info(anime_id)
+    title = api.anime_title()
 
 else:
     print("Que anime quieres ver?")
@@ -87,16 +105,24 @@ for provider in video_links:
     j+=1
 provider = int(input("Proveedor: "))-1
 
-if args.reproduccion_automatica:
-    passive = True
-else:
-    passive_choice = input("Desea activar la reproducción automática?(y/N): ")
-    if passive_choice in ("Yy") and passive_choice!="":
-        passive=True
-    else:
-        passive = False
-#    video=episode_links[episode_list[episode_index]][provider]
-#    os.system(f"mpv ytdl://{video}")
+if args.descarga and args.reproduccion_automatica:
+    parser.error("No se puede ver y descargar el anime al mismo tiempo")
 
-watch_video(episode_list,episode_index,provider,passive)
+
+if args.descarga:
+    dw_all = input("Descargar tambien los siguientes episodios?(y/N): ")
+    if dw_all in ("Yy") and dw_all!="":
+        download_video(anime_id,episode_list,episode_index,provider,True)
+    else:
+        download_video(anime_id,episode_list,episode_index,provider,False)
+else:
+    if args.reproduccion_automatica:
+        passive = True
+    else:
+        passive_choice = input("Desea activar la reproducción automática?(y/N): ")
+        if passive_choice in ("Yy") and passive_choice!="":
+            passive=True
+        else:
+            passive = False
+    watch_video(title,episode_list,episode_index,provider,passive)
 
--- animeflv_scraper.py
+++ animeflv_scraper.py
@@ -1,52 +1,53 @@
 import requests
 import re
 
+#Uncomment all lines and comment the previous one to each to switch the page from m.animeflv.net tp www1.animeflv.ws in case it gets blocked
+
 class Animeflv(object):
 
     def search(self,query):
 
         http = requests.get(f"https://m.animeflv.net/browse?q={query}").text
-        matches=re.findall(r"href=['\"]\/anime\/(.*?)['\"]",http)
+        #http = requests.get(f"https://www1.animeflv.ws/browse?q={query}").text
+        matches=re.findall(r"a href=['\"]\/anime\/(.*?)['\"]",http)
         return matches
 
     def anime_info(self,anime_id):
         
         http= requests.get(f"https://m.animeflv.net/anime/{anime_id}").text
+        #http= requests.get(f"https://www1.animeflv.ws/anime/{anime_id}").text
         try:
             self.title=re.findall(r"h1 class=\"Title\"[>](.*?)[<]",http)[0]
+            #self.title=re.findall(r"h2 class=\"Title\"[>](.*?)[<]",http)[0]
         except:
             self.title=anime_id
         try:
             self.status=re.findall(r'<p><strong>Estado:<\/strong> <strong class="[^"]*">(.*?)<\/strong>',http)[0]
+            #self.status=re.findall(r'<p class="AnmStts "><span class="fa-tv">([^<]+)</span></p>',http)[0]
         except:
             self.status="Not Found"
         try:
             self.summary=re.findall(r"<p><strong>Sinopsis:<\/strong>([\s\S]*?)<",http)[0]
+            #self.summary=re.findall(r"div class=\"Description\"[>](.*?)[<]",http)[0]
         except:
             self.summary="Not Found"
         self.episodes=re.findall(r'href="/ver/([^"]+)"',http)
+        #self.episodes=[anime_id + episode for episode in (re.findall(fr'href="/{anime_id}([^"]+)"',http))][::-1]
     
 
     def get_links(self,episode_to_watch):
-        episodes = self.episodes[episode_to_watch-1]
+        episode = self.episodes[episode_to_watch-1]
         episode_links = {}
-        if type(episodes)==str:
-            http_fragment = requests.get(f"https://m.animeflv.net/ver/{episodes}").text
-            url_pattern = r"https:\\\/\\\/ok.ru\\\/videoembed\\\/.*?\"|https:\\\/\\\/www.yourupload.com\\\/embed\\\/.*?\"|https:\\\/\\\/streamwish.to\\\/e\\\/.*?\""
+        http_fragment = requests.get(f"https://m.animeflv.net/ver/{episode}").text
+        #http_fragment = requests.get(f"https://www1.animeflv.ws/{episode}").text
 
-            links = re.findall(url_pattern, http_fragment)
-            for i in range(len(links)):
-                links[i] = links[i].replace("\\","").strip('"')
-                episode_links[episodes] = links            
-        else:
-            for episode in episodes:
-                http_fragment = requests.get(f"https://m.animeflv.net/ver/{episode}").text
-                url_pattern = r"https:\\\/\\\/ok.ru\\\/videoembed\\\/.*?\"|https:\\\/\\\/www.yourupload.com\\\/embed\\\/.*?\"|https:\\\/\\\/streamwish.to\\\/e\\\/.*?\""
+        url_pattern = r"https:\\\/\\\/ok.ru\\\/videoembed\\\/.*?\"|https:\\\/\\\/www.yourupload.com\\\/embed\\\/.*?\"|https:\\\/\\\/streamwish.to\\\/e\\\/.*?\""
+        #url_pattern = r"https:\/\/ok.ru\/videoembed\/.*?\"|https:\/\/www.yourupload.com\/embed\/.*?\"|https:\/\/streamwish.to\/e\/.*?\"|https:\/\/wishembed.pro\/e\/.*?\""
 
-                links = re.findall(url_pattern, http_fragment)
-                for i in range(len(links)):
-                    links[i] = links[i].replace("\\","").strip('"')
-                    episode_links[episode] = links
+        links = re.findall(url_pattern, http_fragment)
+        for i in range(len(links)):
+            links[i] = links[i].replace("\\","").strip('"')
+            episode_links[episode] = links 
         return episode_links
     
     def anime_title(self):
